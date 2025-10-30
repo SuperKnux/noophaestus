5f1cd6c73e657262fe0a41e952f2d2f5ce80ad04
Ski <34802236+SuperKnux@users.noreply.github.com>
2025-10-29 22:40:17 -0500

    Fixing mixins!

--- Common/src/main/java/at/ski/noophaestus/mixin/MixinItemStack.java
+++ Common/src/main/java/at/ski/noophaestus/mixin/MixinItemStack.java
@@ -15,6 +15,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public class MixinItemStack implements ItemStackAccessor {
@@ -24,17 +26,13 @@ public class MixinItemStack implements ItemStackAccessor {
     @Unique
     private static final String TAG_ENCH_GROUP = "EnchantmentGroups";
 
-    @Definition(id = "tag", field = "Lnet/minecraft/world/item/ItemStack;tag:Lnet/minecraft/nbt/CompoundTag;")
-    @Definition(id = "contains", method = "Lnet/minecraft/nbt/CompoundTag;contains(Ljava/lang/String;I)Z")
-    @Expression("this.tag.contains('Enchantments', 9)")
-    @ModifyExpressionValue(method = "isEnchanted", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private boolean noophaestus$isEnchantedFirstBool(boolean original) {
-        return original || tag.contains(TAG_ENCH_GROUP, 9);
-    }
-
-    @ModifyReturnValue(method = "isEnchanted", at = @At(value = "RETURN", ordinal = 0))
-    private boolean noophaestus$isEnchantedModifyReturn(boolean original) {
-        return original || !tag.getList(TAG_ENCH_GROUP, 10).isEmpty();
+    @Inject(method = "isEnchanted", at = @At("RETURN"), cancellable = true)
+    private void noophaestus$isEnchantedInjectReturn(CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValue()) {
+            if (tag != null && tag.contains(TAG_ENCH_GROUP, 9)) {
+                cir.setReturnValue(!tag.getList(TAG_ENCH_GROUP, 10).isEmpty());
+            }
+        }
     }
 
     @Override
